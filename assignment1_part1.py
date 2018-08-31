7ba5fcdef6747c0a624e15b93427b2b975d10d74
Koon Tse <cepetece@gmail.com>
2018-08-31 17:50:28 -0400

    Run pylint and corrected formatting.

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 """IS211 Assignment1-Part 1"""
 
-def listDivide(numbers = [], divide = 2):
-    """A function returns the number of elements in the 
+def listDivide(numbers=[], divide=2):
+    """A function returns the number of elements in the
 	numbers list parameter that are divisible by the
 	divide integer parameter.
     Args:
         numbers (list): List if integers
         divide (int, default = 2):divisor default value of 2
     Returns:
-        count (int): Count of a number within the list that 
+        count (int): Count of a number within the list that
         can be divided by divide divisor
     Example:
         >>> listDivide([1, 2, 3, 4, 5])
@@ -28,27 +28,26 @@ class ListDivideException(Exception):
     """A custom exceptions class."""
     def __init__(self, msg):
         self.msg = msg
-    
+
 def testListDivide():
     """Test for listDivide function that does not return anything if
 	the result is true.  If the result is false, it will raise the exception.
 	"""
-    
-    result = listDivide([1,2,3,4,5])
+
+    result = listDivide([1, 2, 3, 4, 5])
     if result != 2:
         raise ListDivideException("Test Failed")
-    result = listDivide([2,4,6,8,10])
+    result = listDivide([2, 4, 6, 8, 10])
     if result != 5:
         raise ListDivideException("Test Failed")
-    result = listDivide([30,54,63,98,100], divide=10)
+    result = listDivide([30, 54, 63, 98, 100], divide=10)
     if result != 2:
         raise ListDivideException("Test Failed")
     result = listDivide([])
     if result != 0:
         raise ListDivideException("Test Failed")
-    result = listDivide([1,2,3,4,5],1)
+    result = listDivide([1, 2, 3, 4, 5], 1)
     if result != 5:
         raise ListDivideException("Test Failed")
 
 testListDivide()
-
